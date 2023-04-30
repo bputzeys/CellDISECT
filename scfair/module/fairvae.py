e940386079c8c3c07579fcb5dab821ba35081834
1234K4321 <79265150+1234K4321@users.noreply.github.com>
2023-04-30 21:12:02 +0330

    Update fairvae.py

--- scfair/module/fairvae.py
+++ scfair/module/fairvae.py
@@ -16,10 +16,15 @@ from scvi.nn import DecoderSCVI, Decoder, Encoder, _base_components, one_hot
 
 torch.backends.cudnn.benchmark = True
 
-from scvi_dev.nn._base_components_utils import *
+# from scvi_dev.nn._base_components_utils import *
 from scvi_dev.nn._utils import *
 
-dim_indices = 0  # TODO: 0 might be wrong
+dim_indices = 0
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# device = 'cuda'
+
+# all tensors are samples_count x cov_count
 
 
 class fairVAE(BaseModuleClass):
@@ -83,6 +88,8 @@ class fairVAE(BaseModuleClass):
             use_batch_norm: Tunable[Literal["encoder", "decoder", "none", "both"]] = "both",
             use_layer_norm: Tunable[Literal["encoder", "decoder", "none", "both"]] = "none",
             var_activation: Optional[Callable] = None,
+            alpha: int = 1,  # coef for P(Si|Zi)
+            beta: int = 1,  # coef for TC term
     ):
         super().__init__()
         self.dispersion = "gene"
@@ -93,7 +100,10 @@ class fairVAE(BaseModuleClass):
         # Automatically deactivate if useless
         self.latent_distribution = latent_distribution
 
-        self.px_r = torch.nn.Parameter(torch.randn(n_input))
+        self.alpha = alpha
+        self.beta = beta
+
+        self.px_r = torch.nn.Parameter(torch.randn(n_input)).to(device)
 
         use_batch_norm_encoder = use_batch_norm == "encoder" or use_batch_norm == "both"
         use_batch_norm_decoder = use_batch_norm == "decoder" or use_batch_norm == "both"
@@ -118,7 +128,7 @@ class fairVAE(BaseModuleClass):
             use_layer_norm=use_layer_norm_encoder,
             var_activation=var_activation,
             return_dist=True,
-        )
+        ).to(device)
 
         self.zs_num = len(self.n_cat_list) + n_continuous_cov
         # TODO: should be changed (e.g. some cont_covs (pc_i) might be grouped to 1 zs)
@@ -138,14 +148,14 @@ class fairVAE(BaseModuleClass):
                     use_layer_norm=use_layer_norm_encoder,
                     var_activation=var_activation,
                     return_dist=True,
-                )
+                ).to(device)
                 for _ in range(self.zs_num)
             ]
         )
 
         # Decoders
         # covs are not used in decoders input
-        
+
         self.n_latent = n_latent_shared + n_latent_attribute * self.zs_num
         self.x_decoder = DecoderSCVI(
             self.n_latent,
@@ -156,7 +166,7 @@ class fairVAE(BaseModuleClass):
             use_batch_norm=use_batch_norm_decoder,
             use_layer_norm=use_layer_norm_decoder,
             scale_activation="softmax",
-        )
+        ).to(device)
         self.n_cov_list = [1 if self.is_index_for_cont_cov(i) else self.n_cat_list[i] for i in range(self.zs_num)]
         # TODO: should be changed (e.g. some cont_covs (pc_i) might be grouped to 1 zs)
 
@@ -172,7 +182,7 @@ class fairVAE(BaseModuleClass):
                         use_batch_norm=use_batch_norm_decoder,
                         use_layer_norm=use_layer_norm_decoder,
                         use_activation=True,
-                    )
+                    ).to(device)
                 )
             else:
                 self.s_decoders_list.append(
@@ -185,48 +195,53 @@ class fairVAE(BaseModuleClass):
                         use_batch_norm=use_batch_norm_decoder,
                         use_layer_norm=use_layer_norm_decoder,
                         scale_activation="softmax",
-                    )
+                    ).to(device)
                 )
 
-        self.ps_r = [torch.nn.Parameter(torch.randn(self.n_cov_list[i])) for i in range(self.zs_num)]
+        self.ps_r = [torch.nn.Parameter(torch.randn(self.n_cov_list[i])).to(device) for i in range(self.zs_num)]
 
     def is_index_for_cont_cov(self, i):
         return i >= len(self.n_cat_list)
 
-    def _get_inference_input(self, tensors):
+    def _get_inference_input(self, tensors, for_train=True):
         cont_key = REGISTRY_KEYS.CONT_COVS_KEY
-        cont_covs_tot = tensors[cont_key] if cont_key in tensors.keys() else None
+        cont_covs_tot = tensors[cont_key].to(device) if cont_key in tensors.keys() else None
 
         cat_key = REGISTRY_KEYS.CAT_COVS_KEY
-        cat_covs_tot = tensors[cat_key] if cat_key in tensors.keys() else None
+        cat_covs_tot = tensors[cat_key].to(device) if cat_key in tensors.keys() else None
 
-        x_tot = tensors[REGISTRY_KEYS.X_KEY]
+        x_tot = tensors[REGISTRY_KEYS.X_KEY].to(device)
 
-        # print(0)
-        indices, indices_cf = get_paired_indices(cont_covs_tot, cat_covs_tot, dim_indices)
-        # print(1)
-        
-#         k = cat_covs_tot.size(dim=dim_indices)
-#         indices, indices_cf = torch.tensor(range(k // 2)), torch.tensor(range(k // 2, 2 * (k // 2)))
+        batch_size = cat_covs_tot.size(dim=dim_indices)
+        indices = torch.tensor(list(range(batch_size))).to(device)
+        indices_cf = torch.tensor(list(range(batch_size))).to(device)
+        if for_train:
+            # indices, indices_cf = get_paired_indices(cont_covs_tot, cat_covs_tot, dim_indices)
+            indices = torch.tensor(list(range(batch_size // 2))).to(device)
+            indices_cf = torch.tensor(list(range(batch_size // 2, batch_size))).to(device)
 
         x = torch.index_select(x_tot, dim=dim_indices, index=indices)
         x_cf = torch.index_select(x_tot, dim=dim_indices, index=indices_cf)
 
-        cont_covs = torch.index_select(cont_covs_tot, dim=dim_indices, index=indices) if cont_covs_tot is not None else None
-        cont_covs_cf = torch.index_select(cont_covs_tot, dim=dim_indices, index=indices_cf) if cont_covs_tot is not None else None
+        cont_covs = torch.index_select(cont_covs_tot, dim=dim_indices,
+                                       index=indices).to(device) if cont_covs_tot is not None else None
+        cont_covs_cf = torch.index_select(cont_covs_tot, dim=dim_indices,
+                                          index=indices_cf).to(device) if cont_covs_tot is not None else None
 
-        cat_covs = torch.index_select(cat_covs_tot, dim=dim_indices, index=indices) if cat_covs_tot is not None else None
-        cat_covs_cf = torch.index_select(cat_covs_tot, dim=dim_indices, index=indices_cf) if cat_covs_tot is not None else None
+        cat_covs = torch.index_select(cat_covs_tot, dim=dim_indices,
+                                      index=indices).to(device) if cat_covs_tot is not None else None
+        cat_covs_cf = torch.index_select(cat_covs_tot, dim=dim_indices,
+                                         index=indices_cf).to(device) if cat_covs_tot is not None else None
 
         input_dict = {
-            "x": x,
-            "x_cf": x_cf,
+            "x": x.to(device),
+            "x_cf": x_cf.to(device),
             "cont_covs": cont_covs,
             "cont_covs_cf": cont_covs_cf,
             "cat_covs": cat_covs,
             "cat_covs_cf": cat_covs_cf,
-            "indices": indices,
-            "indices_cf": indices_cf
+            "indices": indices.to(device),
+            "indices_cf": indices_cf.to(device)
         }
         return input_dict
 
@@ -262,8 +277,9 @@ class fairVAE(BaseModuleClass):
                   cat_covs, cat_covs_cf,
                   indices, indices_cf,
                   nullify_cat_covs_indices: Optional[List[int]] = None,
-                  nullify_cont_covs_indices: Optional[List[int]] = None, 
-                  nullify_shared: Optional[bool] = False):
+                  nullify_cont_covs_indices: Optional[List[int]] = None,
+                  nullify_shared: Optional[bool] = False,
+                  for_train=True):
 
         nullify_cat_covs_indices = [] if nullify_cat_covs_indices is None else nullify_cat_covs_indices
         nullify_cont_covs_indices = [] if nullify_cont_covs_indices is None else nullify_cont_covs_indices
@@ -296,26 +312,37 @@ class fairVAE(BaseModuleClass):
 
         qz_shared, z_shared = self.z_shared_encoder(encoder_input, *categorical_input)
         qz_shared_cf, z_shared_cf = self.z_shared_encoder(encoder_input_cf, *categorical_input_cf)
+        z_shared = z_shared.to(device)
+        z_shared_cf = z_shared_cf.to(device)
 
         encoders_outputs = [zs_encoder(encoder_input, *categorical_input) for zs_encoder in self.zs_encoders_list]
         qzs = [enc_out[0] for enc_out in encoders_outputs]
-        zs = [enc_out[1] for enc_out in encoders_outputs]
+        zs = [enc_out[1].to(device) for enc_out in encoders_outputs]
 
-        encoders_outputs_cf = [zs_encoder(encoder_input_cf, *categorical_input_cf) for zs_encoder in self.zs_encoders_list]
+        encoders_outputs_cf = [zs_encoder(encoder_input_cf, *categorical_input_cf) for zs_encoder in
+                               self.zs_encoders_list]
         qzs_cf = [enc_out[0] for enc_out in encoders_outputs_cf]
-        zs_cf = [enc_out[1] for enc_out in encoders_outputs_cf]
+        zs_cf = [enc_out[1].to(device) for enc_out in encoders_outputs_cf]
+
+        # nullify if required
+
+        if nullify_shared:
+            z_shared = torch.zeros_like(z_shared).to(device)
+            z_shared_cf = torch.zeros_like(z_shared_cf).to(device)
 
         for i in range(self.zs_num):
             if ((i - len(self.n_cat_list)) in nullify_cont_covs_indices) or (i in nullify_cat_covs_indices):
-                zs[i] = torch.zeros_like(zs[i])
-                zs_cf[i] = torch.zeros_like(zs_cf[i])
+                zs[i] = torch.zeros_like(zs[i]).to(device)
+                zs_cf[i] = torch.zeros_like(zs_cf[i]).to(device)
 
         zs_concat_f = torch.cat(zs, dim=-1)
         z_concat_f = torch.cat([z_shared, zs_concat_f], dim=-1)
         zs_concat_cf = torch.cat(zs_cf, dim=-1)
         z_concat_cf = torch.cat([z_shared_cf, zs_concat_cf], dim=-1)
 
-        z_concat = torch.cat([z_concat_f, z_concat_cf], dim=0)
+        z_concat = z_concat_f
+        if for_train:
+            z_concat = torch.cat([z_concat_f, z_concat_cf], dim=0)
 
         output_dict = {
             "z_shared": z_shared,
@@ -339,6 +366,53 @@ class fairVAE(BaseModuleClass):
         }
         return output_dict
 
+    def construct_zs_cf(self, zs, cont_covs, cat_covs, cont_covs_cf, cat_covs_cf):
+        # construct counterfactual zs with the same covs as (cont_covs_cf, cat_covs_cf)
+        # approach: take mean of zs that have the same covs as (cont_covs_cf, cat_covs_cf)
+        # if no such zs exists at a point we fill with 0
+        # not: must have dim(covs_cf) << dim(covs)
+
+        zs_cf = []
+
+        for cov_idx in range(len(zs)):
+            zs_cf_cov = []
+            # for sample_idx in range(len(zs[0])):
+            for sample_idx in range(len(cat_covs_cf)):
+
+                if self.is_index_for_cont_cov(cov_idx):
+                    cont_cov_idx = cov_idx - len(self.n_cat_list)
+                    zs_cf_i_all = [zs[cov_idx][j] for j in range(len(zs[cov_idx]))
+                                   if cont_covs[j][cont_cov_idx] == cont_covs_cf[sample_idx][cont_cov_idx]]
+                else:
+                    zs_cf_i_all = [zs[cov_idx][j] for j in range(len(zs[cov_idx]))
+                                   if cat_covs[j][cov_idx] == cat_covs_cf[sample_idx][cov_idx]]
+
+                zs_cf_i_tensor = torch.tensor([list(z) for z in zs_cf_i_all]) if len(zs_cf_i_all) > 0 \
+                    else torch.zeros(len(zs[cov_idx][0]))
+
+                # zs_cf_i_tensor = torch.cat(zs_cf_i_all, dim=0) if len(zs_cf_i_all) > 0 \
+                #     else torch.zeros(1, len(zs[cov_idx][0]))
+                    # else torch.zeros_like(zs[cov_idx][0])
+                zs_cf_i_tensor = zs_cf_i_tensor.to(device)
+
+                # print(zs_cf_i_tensor)
+
+                zs_cf_i = torch.mean(zs_cf_i_tensor, dim=0) if len(zs_cf_i_all) > 0 \
+                    else zs_cf_i_tensor
+                zs_cf_i = zs_cf_i.to(device)
+
+                # print(zs_cf_i)
+                # print(zs_cf_i.size())
+
+                zs_cf_cov.append(zs_cf_i)
+
+            # print(zs_cf_cov)
+
+            zs_cf.append(torch.tensor([list(z) for z in zs_cf_cov]).to(device))
+            # zs_cf.append(torch.cat(zs_cf_cov, dim=0))
+
+        return zs_cf
+
     @auto_move_data
     def generative(self, z_shared, z_shared_cf,
                    zs, zs_cf,
@@ -350,10 +424,14 @@ class fairVAE(BaseModuleClass):
         output_dict = {}
         for i in [0, 1]:
             # p(x|z), p(x|z')
-            z_shared_i = [z_shared, z_shared_cf][i]
-            # TODO: check if we should use above line or below
-            # z_shared_i = z_shared
-            zs_i = [zs, zs_cf][i]
+
+            z_shared_i = z_shared
+
+            if i == 0:
+                zs_i = zs
+            else:
+                zs_i = self.construct_zs_cf(zs, cont_covs, cat_covs, cont_covs_cf, cat_covs_cf)
+
             x_decoder_input = torch.cat([z_shared_i, *zs_i], dim=-1)
             size_factor = [library, library_cf][i]
 
@@ -393,13 +471,13 @@ class fairVAE(BaseModuleClass):
                 ps = Normal(loc=ps_mean, scale=ps_v.sqrt())
             else:
                 # p(s|z)
-                size_factor = library_s[i]
+                size_factor = library_s[i].to(device)
                 ps_scale, ps_r, ps_rate, ps_dropout = s_i_decoder(
                     self.dispersion,
                     zs_i,
                     size_factor,
                 )
-                ps_r = torch.exp(self.ps_r[i])
+                ps_r = torch.exp(self.ps_r[i]).to(device)
                 ps = NegativeBinomial(mu=ps_rate, theta=ps_r, scale=ps_scale)
 
             output_dict["ps"].append(ps)
@@ -415,9 +493,9 @@ class fairVAE(BaseModuleClass):
     ):
         indices = inference_outputs["indices"]
         indices_cf = inference_outputs["indices_cf"]
-        x_tot = tensors[REGISTRY_KEYS.X_KEY]
-        x = torch.index_select(x_tot, dim=dim_indices, index=indices)
-        x_cf = torch.index_select(x_tot, dim=dim_indices, index=indices_cf)
+        x_tot = tensors[REGISTRY_KEYS.X_KEY].to(device)
+        x = torch.index_select(x_tot, dim=dim_indices, index=indices).to(device)
+        x_cf = torch.index_select(x_tot, dim=dim_indices, index=indices_cf).to(device)
 
         # reconstruction losses
 
@@ -425,7 +503,7 @@ class fairVAE(BaseModuleClass):
         reconst_loss_x_cf = -generative_outputs["px_cf"].log_prob(x_cf).sum(-1)
 
         cont_covs = inference_outputs["cont_covs"]
-        cont_input = torch.split(cont_covs, 1, dim=1)
+        cont_input = torch.split(cont_covs, 1, dim=1) if cont_covs is not None else None
 
         cat_covs = inference_outputs["cat_covs"]
         cat_input = torch.split(cat_covs, 1, dim=1)
@@ -433,9 +511,14 @@ class fairVAE(BaseModuleClass):
         reconst_loss_s = 0
         for i in range(self.zs_num):
             s_i = cont_input[i - len(self.n_cat_list)] if self.is_index_for_cont_cov(i) \
-                else one_hot_cat([self.n_cov_list[i]], cat_input[i])
+                else one_hot_cat([self.n_cov_list[i]], cat_input[i]).to(device)
+
+            # print(s_i.device)
+
             reconst_loss_s_i = -generative_outputs["ps"][i].log_prob(s_i).sum(-1)
             reconst_loss_s += reconst_loss_s_i
+
+        reconst_loss_s *= self.alpha
 
         reconst_loss = torch.mean(reconst_loss_x) + torch.mean(reconst_loss_x_cf) + torch.mean(reconst_loss_s)
 
@@ -457,4 +540,3 @@ class fairVAE(BaseModuleClass):
         return LossOutput(
             loss=loss, reconstruction_loss=reconst_loss_x, kl_local=weighted_kl_local
         )
-
