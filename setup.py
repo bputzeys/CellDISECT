519dd0fa9d392d9c8d7dbeece0ca9ec3e1944f1b
1234K4321 <79265150+1234K4321@users.noreply.github.com>
2023-05-05 15:49:51 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='scfair',
+    version='0.0.1',
+    author='Kian Shamsaie',
+    author_email='kianshamsaie@gmail.com',
+    url='https://github.com/1234K4321/scfair',
+    packages=['scfair']
+)
