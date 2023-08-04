ec17e1cb694ef0f4cb18108966bc38d9c4cd8ebc
1234K4321 <79265150+1234K4321@users.noreply.github.com>
2023-08-04 15:14:30 +0330

    Update fairvi_datasplitter.py

--- scfair/fairvi_datasplitter.py
+++ scfair/fairvi_datasplitter.py
@@ -3,13 +3,12 @@ from typing import Dict, List, Optional, Union
 import numpy as np
 
 from scvi.data import AnnDataManager
-from scvi.dataloaders._concat_dataloader import ConcatDataLoader
 from scvi.dataloaders._data_splitting import DataSplitter
+
+from .fairvi_dataloader import FairVIDataLoader
 
 
 class FairVIDataSplitter(DataSplitter):
-
-    data_loader_cls = ConcatDataLoader
 
     def __init__(
         self,
@@ -21,13 +20,13 @@ class FairVIDataSplitter(DataSplitter):
         **kwargs,
     ):
         super().__init__(adata_manager)
-        self.data_loader_class = ConcatDataLoader
+        self.data_loader_cls = FairVIDataLoader
 
     def train_dataloader(self):
-        """Create the train data loader."""
-        return self.data_loader_class(
+        """Create train data loader."""
+        return self.data_loader_cls(
             self.adata_manager,
-            indices_list=[np.asarray(self.train_idx), np.asarray(self.train_idx)],
+            indices=self.train_idx,
             shuffle=True,
             drop_last=False,
             pin_memory=self.pin_memory,
@@ -35,11 +34,11 @@ class FairVIDataSplitter(DataSplitter):
         )
 
     def val_dataloader(self):
-        """Create the validation data loader."""
+        """Create validation data loader."""
         if len(self.val_idx) > 0:
-            return self.data_loader_class(
+            return self.data_loader_cls(
                 self.adata_manager,
-                indices_list=[np.asarray(self.val_idx), np.asarray(self.val_idx)],
+                indices=self.val_idx,
                 shuffle=False,
                 drop_last=False,
                 pin_memory=self.pin_memory,
@@ -49,11 +48,11 @@ class FairVIDataSplitter(DataSplitter):
             pass
 
     def test_dataloader(self):
-        """Create the test data loader."""
+        """Create test data loader."""
         if len(self.test_idx) > 0:
-            return self.data_loader_class(
+            return self.data_loader_cls(
                 self.adata_manager,
-                indices_list=[np.asarray(self.test_idx), np.asarray(self.test_idx)],
+                indices=self.test_idx,
                 shuffle=False,
                 drop_last=False,
                 pin_memory=self.pin_memory,
