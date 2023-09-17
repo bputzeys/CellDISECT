7e0abe29c95ff5aa1ab88b9ac97cd3196fadaaba
HesamAsad <asadzadeh.hesam@ut.ac.ir>
2023-09-18 00:10:51 +0330

    Revert "Delete setup.py"
    This reverts commit 367282ee2dd1e4eadd3c41003e7207f67a212d0b.

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='scfair',
+    version='0.0.1',
+    author='Kian Shamsaie',
+    author_email='kianshamsaie@gmail.com',
+    url='https://github.com/M0hammadL/scfair',
+    packages=['scfair']
+)
