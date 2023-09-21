21fc43d25f28fbc25b14772ff2c9da8a922a4796
Stathis Megas <stathismegas@gmail.com>
2023-09-21 10:55:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='scfair',
     version='0.0.1',
-    author='Kian Shamsaie',
+    author='Stathis Megas; Kian Shamsaie',
     author_email='kianshamsaie@gmail.com',
     url='https://github.com/M0hammadL/scfair',
     packages=['scfair']
